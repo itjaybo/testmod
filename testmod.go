c66cae402b10b7a29484256afd124b92f4b0bfb1
strivebo <ishuzb@gmail.com>
2020-07-28 00:12:37 +0800

    修改 Hi 使它支持多语言

--- testmod.go
+++ testmod.go
@@ -1,8 +1,24 @@
 package testmod
 
-import "fmt"
+import (
+    "errors"
+    "fmt" 
+) 
 
-func Hi(name string) string{
-	//return fmt.Sprintf("Hi, %s", name)
-	return fmt.Sprintf("Hi, %s!!!", name)
+// Hi 返回一个 `lang` 语言的问候语
+func Hi(name, lang string) (string, error) {
+    switch lang {
+    case "cn":
+        return fmt.Sprintf("你好，%s！", name), nil
+    case "en":
+        return fmt.Sprintf("Hi, %s!", name), nil
+    case "pt":
+        return fmt.Sprintf("Oi, %s!", name), nil
+    case "es":
+        return fmt.Sprintf("¡Hola, %s!", name), nil
+    case "fr":
+        return fmt.Sprintf("Bonjour, %s!", name), nil
+    default:
+        return "", errors.New("未知的语言")
+    }
 }
